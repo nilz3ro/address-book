91b9ab17dfec97ff0b5e0fd203ea89a6205122cc
nil <nilz3ro@protonmail.com>
2023-05-18 15:53:39 -0500

    testing beet set.

--- index.ts
+++ index.ts
@@ -1,18 +1,37 @@
-import beet from "@metaplex-foundation/beet";
-import beetSolana from "@metaplex-foundation/beet-solana";
-import {Keypair, PublicKey} from "@solana/web3.js";
+// @ts-nocheck
+import * as beet from "@metaplex-foundation/beet";
+import * as beetSolana from "@metaplex-foundation/beet-solana";
+import { Keypair, PublicKey } from "@solana/web3.js";
 
 class AddressBook {
     constructor(
-        readonly entries: Set<number>
+        readonly entries: Set<PublicKey>
     ) {}
 
-    static readonly struct = new beet.BeetStruct<AddressBook>([
-        ["entries", beet.set(beet.u8)]
-    ], (args) => {
-        let s: Set<number> = new Set();
-        // let pk = Keypair.generate().publicKey;
-        s.add(8);
-        return new AddressBook(s)
-    }, "AddressBook");
+    static readonly struct = new beet.FixableBeetStruct<AddressBook>(
+        [
+            ["entries", beet.set(beetSolana.publicKey)]
+        ],
+        (args) => {
+            console.log("args from constructor helper");
+            console.log(args);
+           return  new AddressBook(args.entries!);
+        },
+        "AddressBook"
+    );
 }
+
+let friendKeypair = Keypair.generate();
+let anotherFriendKeypair = Keypair.generate();
+
+let adb = new AddressBook(new Set([friendKeypair.publicKey, anotherFriendKeypair.publicKey]))
+console.log(adb);
+
+
+let [b] = AddressBook.struct.serialize(adb);
+console.log(b);
+
+let [rehydrated] = AddressBook.struct.deserialize(b); 
+
+console.log("rehydrated");
+console.log(rehydrated);
